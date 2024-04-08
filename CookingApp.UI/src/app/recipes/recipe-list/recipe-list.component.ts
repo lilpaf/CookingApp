c2a0503dc2ddfccad529dca7672889a5bb5250fd
Pavel Stoyanov <fokervt19@gmail.com>
2024-04-08 13:44:26 +0300

    Recipe list implemented

--- CookingApp.UI/src/app/recipes/recipe-list/recipe-list.component.ts
+++ CookingApp.UI/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../models/recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a test', 'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*')
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
