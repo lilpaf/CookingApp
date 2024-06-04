9184064900ac886446572f479566dd69c1b885cc
Pavel Stoyanov <fokervt19@gmail.com>
2024-06-04 22:30:14 +0300

    Error message

--- CookingApp.UI/src/app/auth/auth.service.ts
+++ CookingApp.UI/src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SignUpRequestModel } from './models/sign-up-request.model';
 import { SignUpResponseData } from './models/sign-up-response.model';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,9 +13,27 @@ export class AuthService {
   signUp(email: string, password: string) {
     const userData = new SignUpRequestModel(email, password);
 
-    return this.httpClient.post<SignUpResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCKhlb_QGWU4uG__h4bbPZInpJahVpB7qo',
-      userData
-    );
+    return this.httpClient
+      .post<SignUpResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCKhlb_QGWU4uG__h4bbPZInpJahVpB7qo',
+        userData
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An unknown error occurred!';
+
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email exists already';
+              break;
+          }
+
+          return throwError(errorMessage);
+        })
+      );
   }
 }
